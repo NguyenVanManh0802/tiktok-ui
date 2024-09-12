b09d4dbca7efb2cc30eeeffada688f526aff0444
NguyenVanManh0802 <tonymanh12345@gmail.com>
2024-09-12 20:45:27 +0700

    ui cho header

--- src/components/Layout/DefautLayout/Sidebar/index.js
+++ src/components/Layout/DefautLayout/Sidebar/index.js
@@ -1,8 +1,13 @@
- function Sidebar()
- {
-    return (
-        <h2>Sidebar</h2>
-    )
- }
+import classNames from 'classnames/bind';
+import styles from './Sidebar.module.scss';
+const cx = classNames.bind(styles);
 
- export default Sidebar
+function Sidebar() {
+    return (
+        <aside className={cx('wrapper')}>
+            <h2>Sidebar</h2>
+        </aside>
+    );
+}
+
+export default Sidebar;
